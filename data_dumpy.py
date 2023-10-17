499426f9a3e3f2a02a82c41fe3592bbc5424d80b
swethaparmi <swethaparmi97@gmail.com>
2023-10-17 16:09:00 +0530

    setup completed

---
+++ data_dumpy.py
@@ -0,0 +1,21 @@
+import pymongo # pip install pymongo
+import pandas as pd
+import json
+
+client = pymongo.MongoClient("mongodb+srv://swethaparmi97:Shwetha123@cluster0.4ak0k0h.mongodb.net/?retryWrites=true&w=majority")
+
+DATA_FILE_PATH = ("insurance.csv")
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    df.reset_index(drop = True, inplace = True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
